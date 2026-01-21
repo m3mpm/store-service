86e3f964c967d0918971b330a37c340110d8836e
Ruslan <m3mpm@users.noreply.github.com>
2026-01-21 17:51:17 +0700

    feat(db): added liquidbase changelog for initial database schema - added liquibase YAML configuration - defined initial tables for the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,15 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
+// Версии зависимостей
+val mapstructVersion = "1.6.3"
+val springDocVersion = "2.8.0"
+val lombokMapstructBindingVersion = "0.2.0"
+val dotenvVersion = "5.1.0"
+
 group = "org.m3mpm"
 version = "0.0.1-SNAPSHOT"
 description = "Store Service for Spring Boot"
-
-val mapstructVersion = "1.6.3"
 
 java {
 	toolchain {
@@ -37,7 +41,7 @@ dependencies {
     runtimeOnly("org.postgresql:postgresql")
 
     // Swagger / OpenAPI
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
@@ -48,7 +52,7 @@ dependencies {
     annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
 
     // Связка Lombok и MapStruct (чтобы MapStruct видел геттеры/сеттеры Lombok)
-    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+    annotationProcessor("org.projectlombok:lombok-mapstruct-binding:$lombokMapstructBindingVersion")
 
     // DevTools
     developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -60,6 +64,18 @@ dependencies {
     // Добавлено для Lombok в тестах
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
+
+    // env file
+    // Source: https://mvnrepository.com/artifact/me.paulschwarz/springboot4-dotenv
+    implementation("me.paulschwarz:springboot4-dotenv:$dotenvVersion")
+
+    // not work
+    // Source: https://mvnrepository.com/artifact/io.github.cdimascio/dotenv-java
+//    implementation("io.github.cdimascio:dotenv-java:3.2.0")
+
+    // not work
+    // Source: https://mvnrepository.com/artifact/io.github.cdimascio/dotenv-kotlin
+//    runtimeOnly("io.github.cdimascio:dotenv-kotlin:6.5.1")
 }
 
 tasks.withType<Test> {
@@ -76,7 +92,7 @@ tasks.withType<JavaCompile> {
         // Прерывает сборку, если есть несопоставленные поля — предотвращает "тихие" ошибки маппинга
         "-Amapstruct.unmappedTargetPolicy=ERROR",
 
-//        "-Xlint:all,-processing"
+//        "-Xlint:all,-processing" // Включает все проверки компилятора на чистоту кода
 
     ))
 }
